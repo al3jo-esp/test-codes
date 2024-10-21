e04db6ea51848036cba746850122acde906cdcab
al3jo-esp <aleix.fernandez.007@gmail.com>
2024-10-21 11:53:08 +0200

    Update Ej_1.py

--- Ej_1.py
+++ Ej_1.py
@@ -5,3 +5,12 @@ if Username=="Gwen" and Password=="excalibur":
  print("Usuario y contraseña correctos, puedes ingresar.")
 else:
  print("Usuario/contraseña incorrecto. Vuelve a intentarlo!")
+
+Username=input("Nombre de usuario: ")
+Password=input("Contraseña: ")
+while Username!="Gwen" or Password!="excalibur":
+  print("Usuario/contraseña incorrecto. Vuelve a intentarlo!")
+  Username=input("Nombre de usuario: ")
+  Password=input("Contraseña: ")
+else:
+   print("Usuario y contraseña correctos, puedes ingresar.")
